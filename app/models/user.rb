844bc710f03084f3d30c7c6dc615f83e13fc13c6
salman7777in <salmanazhar7777@gmail.com>
2022-11-02 16:35:01 +0500

    complete sign_up form page with phonelib gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: { case_insensitive: true }
+  validates :country, countries_alpha2: true
+  # validates :phone_number, phone: {possible: true, allow_blank: false}, uniqueness: false
+  validates :phone_number, phone: { possible: true, allow_blank: true, types: [:voip, :mobile], country_specifier: -> phone { phone.country.try(:upcase) } }
+  before_save :format_phone
+  def format_phone
+    self.phone_number = Phonelib.parse(phone_number).e164
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  # Home controller
+  # index action
+  # home folder in views
+  # index.html.erb
+  # add logout link by googlin it that how to add logout link
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
