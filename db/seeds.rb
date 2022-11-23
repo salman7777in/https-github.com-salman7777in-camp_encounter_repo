9c32947529d62ed44691b42cb22a4b9edd7aa454
salman7777in <salmanazhar7777@gmail.com>
2022-11-23 19:48:35 +0500

    create Admin seed file with emil and password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin= Admin.find_or_create_by(email: 'salman123@gmail.com', encrypted_password: 'Inspiron600#')
+puts "Created #{Admin.count} ADMIN"
